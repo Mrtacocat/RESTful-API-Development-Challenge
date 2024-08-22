8367a3ec2c48adceda1bbd4b27b52eb8366f5a1f
Christoffer Riis <33607859+Mrtacocat@users.noreply.github.com>
2024-08-22 11:25:35 +0200

    create database context

---
+++ Restful API Development Challenge/Controllers/BooksController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Restful_API_Development_Challenge.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+    }
+}
---
+++ Restful API Development Challenge/Data/LibraryContext.cs
@@ -0,0 +1,23 @@
+﻿namespace Restful_API_Development_Challenge.Data;
+
+using Microsoft.EntityFrameworkCore;
+
+
+
+public class LibraryContext : DbContext
+{
+    public LibraryContext(DbContextOptions<LibraryContext> options) : base(options) { }
+
+    public DbSet<Book> Books { get; set; }
+}
+
+public class Book
+{
+    public int BookId { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string ISBN { get; set; }
+    public DateTime PublishedDate { get; set; }
+    public string Status { get; set; }
+    public DateTime? BorrrowedUntil { get; set; }
+}
